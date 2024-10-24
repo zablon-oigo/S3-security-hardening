afddc1142846fbc90ff3ef3f6b29051713289402
zablon <zablonoigo51@gmail.com>
2024-10-24 21:18:11 +0300

    Add s3 bucket ownership

--- main.tf
+++ main.tf
@@ -17,3 +17,9 @@ resource "aws_s3_bucket" "bucket" {
     Terraform   = "true"
   }
 }
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
