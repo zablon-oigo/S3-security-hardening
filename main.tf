72eee275a9c8ceaf283cc99d08ca80c8fb1d362d
zablon <zablonoigo51@gmail.com>
2024-10-25 06:54:51 +0300

    add lambda execution role

--- main.tf
+++ main.tf
@@ -34,3 +34,7 @@ data "aws_iam_policy_document" "lambda_role_trust_policy" {
   }
   
 }
+resource "aws_iam_role" "lambda_execution_role"{
+  name="${var.service_name}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_role_trust_policy.json
+}
