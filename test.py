136a4e9837d86f32a0012a5ea8369810db2eac7b
zablon <zablonoigo51@gmail.com>
2024-10-25 06:47:14 +0300

    add lambda  test

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+import tempfile
+
+BUCKET_NAME=os.environ['BUCKET_NAME']
+
+def lambda_handler(event, context):
+    s3_client=boto3.client('s3')
+    file_content='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.'
+    temp_file=tempfile.NamedTemporaryFile(delete=False)
+    with opent(temp_file.name,'w')as f:
+        f.write(file_content)
+
+    s3_client.upload_file(temp_file.name,BUCKET_NAME,'test.txt')
