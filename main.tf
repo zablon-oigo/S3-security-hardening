2f3dd124a8a4cd6915e2564cf5991940ffd4a103
zablon <zablonoigo51@gmail.com>
2024-10-28 17:53:26 +0300

    Add encryption keys

--- main.tf
+++ main.tf
@@ -74,3 +74,14 @@ tags = {
     "Terraform"   = "true"
   }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_config" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+    bucket_key_enabled = true
+  }
+}
