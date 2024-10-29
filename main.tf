8d173e8bc743cb3e3fd764a1e64cca703fed18bc
zablon <zablonoigo51@gmail.com>
2024-10-29 17:09:17 +0300

    Add kms tags

--- main.tf
+++ main.tf
@@ -77,6 +77,50 @@ tags = {
 resource "aws_kms_key" "kms_key" {
   description             = "KMS key for ${var.service_name} in the ${var.environment} environment"
   deletion_window_in_days = 7
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "${var.service_name}-${var.environment}-key-policy",
+    Statement = concat([
+      {
+        Sid    = "Allow administration of the key",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" # allow the root user to administer the key but not use it
+        },
+        Action = [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow use of the key",
+        Effect = "Allow",
+        Principal = {
+          AWS = "${aws_iam_role.service_lambda_execution_role.arn}" # allow the lambda execution role to use the key but not administer it
+        },
+        Action   = ["kms:Decrypt", "kms:DescribeKey", "kms:Encrypt", "kms:GenerateDataKey*", "kms:ReEncrypt*", "kms:CreateGrant"]
+        Resource = "*"
+      }]
+    )
+  })
+
+  tags = {
+    Service     = var.service_name
+    Environment = var.environment
+    Name        = "${var.service_name}-kms-key"
+    Terraform   = "true"
+  }
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse_config" {
   bucket = aws_s3_bucket.bucket.id
