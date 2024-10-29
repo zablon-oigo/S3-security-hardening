0253e9c2b79e8e8ff0d823a31d2b0abad0ea00e9
zablon <zablonoigo51@gmail.com>
2024-10-29 17:03:43 +0300

    attach s3 bucket policy

--- main.tf
+++ main.tf
@@ -102,3 +102,7 @@ data "aws_iam_policy_document" "s3_endpoint_policy" {
     }
   }
 }
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
